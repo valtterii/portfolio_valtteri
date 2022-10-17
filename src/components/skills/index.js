5f3685179dbdabb75fa76e80994a111d9b326116
valtterii <valtteri.karppinen04@gmail.com>
2022-10-17 12:06:44 +0300

    Added skill level indicators and animations

--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -13,32 +13,48 @@ function Skills(props) {
             <h1>{props.title}</h1>
             <div className='skills'>
                 <div className='box' data-aos="zoom-out-right" data-aos-mirror="true" data-aos-once="false">
+                    <div data-aos="fade-up-right" data-aos-mirror="true" data-aos-once="false" data-aos-delay="500" data-aos-duration="500" className='skill-level'></div>
                     HTML
                 </div>
                 <div className='box' data-aos="zoom-out-up" data-aos-delay="200" data-aos-mirror="true" data-aos-once="false">
+                    <div data-aos="fade-up" data-aos-mirror="true" data-aos-once="false" data-aos-delay="500" data-aos-duration="500" className='skill-level'></div>
                     CSS
                 </div>
                 <div className='box' data-aos="zoom-out-left" data-aos-mirror="true" data-aos-once="false">
+                    <div data-aos="fade-up-left" data-aos-mirror="true" data-aos-once="false" data-aos-delay="500" data-aos-duration="500" className='skill-level novice'></div>
                     React
                 </div>
                 <div className='box' data-aos="zoom-out-up" data-aos-delay="400" data-aos-mirror="true" data-aos-once="false">
+                    <div data-aos="fade-up" data-aos-mirror="true" data-aos-once="false" data-aos-delay="500" data-aos-duration="500" className='skill-level'></div>
                     WordPress
                 </div>
                 <div className='box' data-aos="zoom-out-up" data-aos-delay="500" data-aos-mirror="true" data-aos-once="false">
+                    <div data-aos="fade-up" data-aos-mirror="true" data-aos-once="false" data-aos-delay="500" data-aos-duration="500" className='skill-level novice'></div>
                     Python
                 </div>
                 <div className='box' data-aos="zoom-out-up" data-aos-delay="600" data-aos-mirror="true" data-aos-once="false">
+                    <div data-aos="fade-up" data-aos-mirror="true" data-aos-once="false" data-aos-delay="500" data-aos-duration="500" className='skill-level novice'></div>
                     PHP
                 </div>
                 <div className='box' data-aos="zoom-out-up" data-aos-delay="700" data-aos-mirror="true" data-aos-once="false">
+                    <div data-aos="fade-up" data-aos-mirror="true" data-aos-once="false" data-aos-delay="500" data-aos-duration="500" className='skill-level novice'></div>
                     MySQL
                 </div>
                 <div className='box' data-aos="zoom-out-up" data-aos-delay="800" data-aos-mirror="true" data-aos-once="false">
+                    <div data-aos="fade-up" data-aos-mirror="true" data-aos-once="false" data-aos-delay="500" data-aos-duration="500" className='skill-level'></div>
                     Git
                 </div>
                 <div className='box' data-aos="zoom-out-up" data-aos-delay="900" data-aos-mirror="true" data-aos-once="false">
+                    <div data-aos="fade-up" data-aos-mirror="true" data-aos-once="false" data-aos-delay="500" data-aos-duration="500" className='skill-level'></div>
                     Selenium IDE
                 </div>
+            </div>
+            <div data-aos="fade-up" data-aos-mirror="true" data-aos-once="false" data-aos-delay="500" data-aos-duration="1500" className='explanations'>
+                <div data-aos="fade-up" data-aos-mirror="true" data-aos-once="false" data-aos-delay="500" data-aos-duration="500" className='skill-level'></div>
+                <span>Intermediate / Advanced</span>
+                <br />
+                <div data-aos="fade-up" data-aos-mirror="true" data-aos-once="false" data-aos-delay="500" data-aos-duration="500" className='skill-level novice' style={{background: "rgb(226, 144, 21)"}}></div>
+                <span>Novice</span>
             </div>
         </div>
         </>
