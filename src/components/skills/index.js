1392a7a38735ff4e196cc2ceadaea1c0e1a32217
valtterii <valtteri.karppinen04@gmail.com>
2022-10-17 21:48:12 +0300

    improved code

--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -12,39 +12,39 @@ function Skills(props) {
         <div className='section_content'>
             <h1>{props.title}</h1>
             <div className='skills'>
-                <div className='box' data-aos="zoom-out-right" data-aos-mirror="true" data-aos-once="false">
+                <div className='grid-item1 box' data-aos="zoom-out-right" data-aos-mirror="true" data-aos-once="false">
                     <div data-aos="fade-up-right" data-aos-mirror="true" data-aos-once="false" data-aos-delay="500" data-aos-duration="500" className='skill-level'></div>
                     HTML
                 </div>
-                <div className='box' data-aos="zoom-out-up" data-aos-delay="200" data-aos-mirror="true" data-aos-once="false">
+                <div className='grid-item2 box' data-aos="zoom-out-up" data-aos-delay="200" data-aos-mirror="true" data-aos-once="false">
                     <div data-aos="fade-up" data-aos-mirror="true" data-aos-once="false" data-aos-delay="500" data-aos-duration="500" className='skill-level'></div>
                     CSS
                 </div>
-                <div className='box' data-aos="zoom-out-left" data-aos-mirror="true" data-aos-once="false">
+                <div className='grid-item3 box' data-aos="zoom-out-left" data-aos-mirror="true" data-aos-once="false">
                     <div data-aos="fade-up-left" data-aos-mirror="true" data-aos-once="false" data-aos-delay="500" data-aos-duration="500" className='skill-level novice'></div>
                     React
                 </div>
-                <div className='box' data-aos="zoom-out-up" data-aos-delay="400" data-aos-mirror="true" data-aos-once="false">
+                <div className='grid-item4 box' data-aos="zoom-out-up" data-aos-delay="400" data-aos-mirror="true" data-aos-once="false">
                     <div data-aos="fade-up" data-aos-mirror="true" data-aos-once="false" data-aos-delay="500" data-aos-duration="500" className='skill-level'></div>
                     WordPress
                 </div>
-                <div className='box' data-aos="zoom-out-up" data-aos-delay="500" data-aos-mirror="true" data-aos-once="false">
+                <div className='grid-item5 box' data-aos="zoom-out-up" data-aos-delay="500" data-aos-mirror="true" data-aos-once="false">
                     <div data-aos="fade-up" data-aos-mirror="true" data-aos-once="false" data-aos-delay="500" data-aos-duration="500" className='skill-level novice'></div>
                     Python
                 </div>
-                <div className='box' data-aos="zoom-out-up" data-aos-delay="600" data-aos-mirror="true" data-aos-once="false">
+                <div className='grid-item6 box' data-aos="zoom-out-up" data-aos-delay="600" data-aos-mirror="true" data-aos-once="false">
                     <div data-aos="fade-up" data-aos-mirror="true" data-aos-once="false" data-aos-delay="500" data-aos-duration="500" className='skill-level novice'></div>
                     PHP
                 </div>
-                <div className='box' data-aos="zoom-out-up" data-aos-delay="700" data-aos-mirror="true" data-aos-once="false">
+                <div className='grid-item7 box' data-aos="zoom-out-up" data-aos-delay="700" data-aos-mirror="true" data-aos-once="false">
                     <div data-aos="fade-up" data-aos-mirror="true" data-aos-once="false" data-aos-delay="500" data-aos-duration="500" className='skill-level novice'></div>
                     MySQL
                 </div>
-                <div className='box' data-aos="zoom-out-up" data-aos-delay="800" data-aos-mirror="true" data-aos-once="false">
+                <div className='grid-item8 box' data-aos="zoom-out-up" data-aos-delay="800" data-aos-mirror="true" data-aos-once="false">
                     <div data-aos="fade-up" data-aos-mirror="true" data-aos-once="false" data-aos-delay="500" data-aos-duration="500" className='skill-level'></div>
                     Git
                 </div>
-                <div className='box' data-aos="zoom-out-up" data-aos-delay="900" data-aos-mirror="true" data-aos-once="false">
+                <div className='grid-item9 box' data-aos="zoom-out-up" data-aos-delay="900" data-aos-mirror="true" data-aos-once="false">
                     <div data-aos="fade-up" data-aos-mirror="true" data-aos-once="false" data-aos-delay="500" data-aos-duration="500" className='skill-level'></div>
                     Selenium IDE
                 </div>
