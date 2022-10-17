596156d3954df596e5560995f451f88fad0f1005
valtterii <valtteri.karppinen04@gmail.com>
2022-10-17 12:24:08 +0300

    Modified footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './main.css'
+import linkedin from '../../images/linkedin.png'
 
 function Footer() {
   return (
     <div className='footer'>
+      <div className='footer_content'>
         <h2> Valtteri Karppinen</h2>
+          <a target={"_blank"} href='https://www.linkedin.com/in/valtteri-karppinen/'>
+            LinkedIn <img className='linkedin' src={linkedin} />
+          </a>
+      </div>
     </div>
   )
 }
