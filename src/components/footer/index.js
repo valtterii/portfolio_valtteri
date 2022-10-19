564856b484ab441c199296dd06a2061ebfb0319a
valtterii <valtteri.karppinen04@gmail.com>
2022-10-19 10:59:21 +0300

    Added footer github link

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import './main.css'
 import linkedin from '../../images/linkedin.png'
+import github from '../../images/github.png'
 
 function Footer() {
   return (
     <div className='footer'>
       <div className='footer_content'>
         <h2> Valtteri Karppinen </h2>
+        <div className='footer_links'>
           <a target={"_blank"} href='https://www.linkedin.com/in/valtteri-karppinen/'>
             LinkedIn <img className='linkedin' src={linkedin} />
           </a>
+          <a target={"_blank"} href='https://github.com/valtterii/portfolio_valtteri'>
+            Github <img className='linkedin' src={github} />
+          </a>
+        </div>
           <p style={{color: "dodgerblue"}}>valtteri.karppinen04(ät)gmail.com</p>
       </div>
     </div>
