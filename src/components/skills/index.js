f2b6a7428b30cfe54b39ebea8ba59b9727f62b3c
valtterii <valtteri.karppinen04@gmail.com>
2022-10-29 21:49:56 +0300

    Cleaned up code

--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -18,40 +18,40 @@ function Skills(props) {
         <>
         <div className='section_content'>
             <h1>{props.title}</h1>
-            <div className='skills'>
-                <div className='grid-item1 box' data-aos={animations.zoom.animation1} data-aos-offset={props.offset}>
+            <div className='skills' data-aos={animations.zoom.animation2} data-aos-offset={props.offset}>
+                <div className='grid-item1 box' data-aos={animations.zoom.animation1} data-aos-offset={props.offset} data-aos-delay={"100"}>
                     <div className='skill-level'></div>
                     HTML
                 </div>
-                <div className='grid-item2 box' data-aos={animations.zoom.animation2} data-aos-offset={props.offset}>
+                <div className='grid-item2 box'>
                     <div className='skill-level'></div>
                     CSS
                 </div>
-                <div className='grid-item3 box' data-aos={animations.zoom.animation3} data-aos-offset={props.offset}>
+                <div className='grid-item3 box' data-aos={animations.zoom.animation3} data-aos-offset={props.offset} data-aos-delay={"100"}>
                     <div className='skill-level novice'></div>
                     React
                 </div>
-                <div className='grid-item4 box' data-aos={animations.zoom.animation2} data-aos-offset={props.offset}>
+                <div className='grid-item4 box'>
                     <div className='skill-level'></div>
                     WordPress
                 </div>
-                <div className='grid-item5 box' data-aos={animations.zoom.animation2} data-aos-offset={props.offset}>
+                <div className='grid-item5 box'>
                     <div className='skill-level novice'></div>
                     Python
                 </div>
-                <div className='grid-item6 box' data-aos={animations.zoom.animation2} data-aos-offset={props.offset}>
+                <div className='grid-item6 box'>
                     <div className='skill-level novice'></div>
                     PHP
                 </div>
-                <div className='grid-item7 box' data-aos={animations.zoom.animation2} data-aos-offset={props.offset}>
+                <div className='grid-item7 box'>
                     <div className='skill-level novice'></div>
                     MySQL
                 </div>
-                <div className='grid-item8 box' data-aos={animations.zoom.animation2} data-aos-offset={props.offset}>
+                <div className='grid-item8 box'>
                     <div className='skill-level'></div>
                     Git
                 </div>
-                <div className='grid-item9 box' data-aos={animations.zoom.animation2} data-aos-offset={props.offset}>
+                <div className='grid-item9 box'>
                     <div className='skill-level'></div>
                     Selenium IDE
                 </div>
@@ -60,7 +60,7 @@ function Skills(props) {
                 <div data-aos={animations.fade.animation1} data-aos-offset={props.offset} className='skill-level'></div>
                 <span>Intermediate / Advanced</span>
                 <br />
-                <div data-aos={animations.fade.animation1} data-aos-delay="500" data-aos-duration="500" data-aos-offset={props.offset} className='skill-level novice' style={{background: "rgb(226, 144, 21)"}}></div>
+                <div data-aos={animations.fade.animation1} data-aos-delay="500" data-aos-duration="500" data-aos-offset={props.offset} className='skill-level novice'></div>
                 <span>Novice</span>
                 <br />
             </div>
