81404c10d4c52d3959854611a231dd36153caba3
valtterii <valtteri.karppinen04@gmail.com>
2022-10-31 15:37:32 +0200

    Modified link

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -12,7 +12,7 @@ function Footer() {
           <a target={"_blank"} href='https://www.linkedin.com/in/valtteri-karppinen/'>
             LinkedIn <img className='logo' src={linkedin} />
           </a>
-          <a target={"_blank"} href='https://github.com/valtterii/portfolio_valtteri'>
+          <a target={"_blank"} href='https://github.com/valtterii/'>
             Github <img className='logo' src={github} />
           </a>
         </div>
